edbace2f89a7b2c0397ac5e948df3e3b3eabe25e
github username <github email address>
2019-02-01 13:34:36 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   get 'about', to: "static#about"
 
-  get '/posts/:id', to: "posts#show"
+  # get '/posts/:id', to: "posts#show"
+  resources :posts, only: :show
+
+
 
 
 
